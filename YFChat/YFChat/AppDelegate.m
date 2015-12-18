fe8c7e9f2eec515f94c4f5f99503bd0694f0b6f7
yongfang <yongfang_lili@163.com>
2015-12-18 16:42:23 +0800

    依赖文件的下载

--- YFChat/YFChat/AppDelegate.m
+++ YFChat/YFChat/AppDelegate.m
@@ -19,24 +19,17 @@
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     
     [[EaseMob sharedInstance] registerSDKWithAppKey:@"yongfang#yfchat" apnsCertName:@"istore_dev"];
-    [[EaseMob sharedInstance] application:application didFinishLaunchingWithOptions:launchOptions];
+//    [[EaseMob sharedInstance] application:application didFinishLaunchingWithOptions:launchOptions];
     
     return YES;
 }
 
-- (void)applicationWillResignActive:(UIApplication *)application {
-    // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
-    // Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
-}
-
 - (void)applicationDidEnterBackground:(UIApplication *)application {
-   
-    [[EaseMob sharedInstance] applicationWillEnterForeground:application];
-
+    [[EaseMob sharedInstance] applicationDidEnterBackground:application];
 }
 
 - (void)applicationWillEnterForeground:(UIApplication *)application {
-    // Called as part of the transition from the background to the inactive state; here you can undo many of the changes made on entering the background.
+    [[EaseMob sharedInstance] applicationWillEnterForeground:application];
 }
 
 - (void)applicationDidBecomeActive:(UIApplication *)application {
@@ -44,10 +37,8 @@
 }
 
 - (void)applicationWillTerminate:(UIApplication *)application {
-    
-     [[EaseMob sharedInstance] applicationWillTerminate:application];
-    
-
+    [[EaseMob sharedInstance] applicationWillTerminate:application];
 }
+
 
 @end
--- YFChat/YFChat/ViewController.m
+++ YFChat/YFChat/ViewController.m
@@ -7,9 +7,7 @@
 //
 
 #import "ViewController.h"
-#import <EaseMob.h>
-
-@interface ViewController ()
+@interface ViewController ()<IChatManagerDelegate>
 @property (weak, nonatomic) IBOutlet UITextField *accountText;
 @property (weak, nonatomic) IBOutlet UITextField *pwdaccount;
 
@@ -26,10 +24,26 @@
     if (_pwdaccount.text.length == 0) {
         return;
     }
+//    [[EaseMob sharedInstance].chatManager asyncLoginWithUsername:_accountText.text password:_pwdaccount.text completion:^(NSDictionary *loginInfo, EMError *error) {
+//        if (!error && loginInfo) {
+//            [HMStatusBarHUD showSuccess:@"登陆成功"];
+//            
+//            NSLog(@"登陆成功 %@",loginInfo);
+//        }
+//        
+//    } onQueue:nil];
     
     [[EaseMob sharedInstance].chatManager asyncLoginWithUsername:_accountText.text password:_pwdaccount.text completion:^(NSDictionary *loginInfo, EMError *error) {
+        if (!error) {
+            [HMStatusBarHUD showSuccess:@"登陆成功"];
+            NSLog(@"登录成功 %@",loginInfo);
+
+        }else{
+             NSLog(@"-------》%@",loginInfo);
+            [HMStatusBarHUD showSuccess:@"登录失败"];
+            NSLog(@"登录失败 %@",error);
         
-        NSLog(@"登录成功 %@",loginInfo);
+        }
     } onQueue:nil];
 }
 
@@ -41,8 +55,10 @@
     [[EaseMob sharedInstance].chatManager asyncRegisterNewAccount:_accountText.text password:_accountText.text withCompletion:^(NSString *username, NSString *password, EMError *error) {
         
         if (!error){
+            [HMStatusBarHUD showSuccess:@"注册成功"];
             NSLog(@"注册成功");
         }else {
+            [HMStatusBarHUD showSuccess:@"注册失败"];
             NSLog(@"注册失败");
         
         }
@@ -54,6 +70,8 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    
+     NSLog(@"打印iOS环信SDK版本号：%@",[EaseMob sharedInstance].sdkVersion);
     // Do any additional setup after loading the view, typically from a nib.
 }
 
