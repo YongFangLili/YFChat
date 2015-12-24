07f0b8514039028e5d3ad5829c1d7ec182e485c5
yongfang <yongfang_lili@163.com>
2015-12-24 12:26:23 +0800

    单例存储用户信息

---
+++ YFChat/YFChat/UserInfoManager.m
@@ -0,0 +1,51 @@
+//
+//  UserInfoManager.m
+//  YFChat
+//
+//  Created by 李永方 on 15/12/23.
+//  Copyright © 2015年 李永方. All rights reserved.
+//
+
+#import "UserInfoManager.h"
+
+@implementation UserInfoManager
++(instancetype)sharedUserInfoManager {
+    
+    static id instance;
+    static dispatch_once_t onceToken;
+    dispatch_once(&onceToken, ^{
+        
+        instance = [[UserInfoManager alloc]init];
+    });
+    
+    return instance;
+}
+
+#warning 需要改善，当添加好友的时候，不能及时刷新
+-(NSArray *)friendsList {
+    
+    if (_friendsList == nil) {
+        
+        //        [[EaseMob sharedInstance].chatManager asyncFetchBuddyList];
+        
+        [[EaseMob sharedInstance].chatManager asyncFetchBuddyListWithCompletion:^(NSArray *buddyList, EMError *error) {
+            if (!error) {
+                NSLog(@"获取成功 -- %@",buddyList);
+                _friendsList = buddyList;
+                
+            }
+            //            for (EMBuddy *budy in self.friends) {
+            //
+            //                NSLog(@"%@",budy);
+            //            }
+        } onQueue:nil];
+        
+        
+    }
+    
+    
+    return _friendsList;
+    
+}
+
+@end
